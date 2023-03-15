16c8c6869a7b57e829886e45b7645fa6e7d07d6e
THIVINKANTH <thivinkanth@gmail.com>
2023-03-15 13:40:29 +0530

    react js landing completed

---
+++ Day3Js/customerusecase/src/App.js
@@ -0,0 +1,12 @@
+import { LandingPage } from "./Landing";
+
+
+function App() {
+  return (
+    <>
+    <LandingPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ Day3Js/customerusecase/src/Loginpage.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+export const Login=()=>
+{
+    return(
+        <>
+        </>
+    )
+}
